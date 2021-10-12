8933f3fd04a4d1f345322a2affa2f4ee1b5c0c00
Phillip Kim <phillipkakg@gmail.com>
2021-10-12 13:35:21 -0700

    Deleted some unneeded files

--- pymu-quickstar.py
+++
@@ -1,6 +0,0 @@
-import fitz
-doc = fitz.open('cp44007.pdf')
-
-page1 = doc[0]
-words = page1.get_text("words")
-print(words)
--- quickstart.py
+++
@@ -1,6 +0,0 @@
-import tabula
-
-url = 'cp44007.pdf'
-table = tabula.read_pdf(url, pages='all', multiple_tables=True)
-
-print(table)
